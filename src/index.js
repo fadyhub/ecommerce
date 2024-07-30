0dd6b622a9c9b741a4a1c6164f7d63dcafe4a194
fadyhub <PUT_YOUR_EMAIL_HERE>
2024-07-30 12:37:46 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css'
+import '@fortawesome/fontawesome-free/js/all.min'
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+console.log("مرحباً بك")
+
+console.log("معلش خد وقتك")
