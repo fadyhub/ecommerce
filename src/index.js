20c60af7c8e2ed92a8546498737f2a9e8813388f
fadyhub <PUT_YOUR_EMAIL_HERE>
2024-08-01 11:05:39 +0300

    most sold product

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css'
 import '@fortawesome/fontawesome-free/js/all.min'
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+document.querySelectorAll('[data-bs-toggle-tooltip]').forEach(item => new bootstrap.Tooltip(item))
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+  item.addEventListener("click", () => {
+    alert("أضيف المنتج إلي عربة الشراء")
   })
-console.log("مرحباً بك")
+})
 
-console.log("معلش خد وقتك")
+console.log("أهلاً بك في متجر عربي")
